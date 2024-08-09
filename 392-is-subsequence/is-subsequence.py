20fe983b66f8b751389fab3f3f077820e34921df
Aparnamol K S <152758476+Aparnamol-KS@users.noreply.github.com>
2024-08-09 23:07:10 +0530

    Time: 14 ms (65.11%) | Memory: 11.8 MB (34.92%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i=0
+        ls = len(s)
+        lt = len(t)
+        if ls>lt:
+            return False
+        elif (ls==lt and s==t) or ls==0:
+            return True
+        for j in t:
+            if s[i]==j:
+                if i+1==ls:
+                    return True
+                i+=1
+        return False
+                
