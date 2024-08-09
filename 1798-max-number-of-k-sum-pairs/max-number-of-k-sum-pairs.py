6464ae4f99073fc67684e398e79fa6cb375bd239
Aparnamol K S <152758476+Aparnamol-KS@users.noreply.github.com>
2024-08-09 22:39:01 +0530

    Time: 523 ms (28.98%) | Memory: 21.6 MB (31.63%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        arr = dict()
+        count = 0
+        for i in nums:
+            if arr.get(k-i,0)==0:
+                arr[i] = arr.get(i,0)+1
+                continue
+            count +=1
+            arr[k-i]-=1
+        return count
