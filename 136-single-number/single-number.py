706707867bd859178c412c7862b5a40261ab36a7
Aparnamol K S <152758476+Aparnamol-KS@users.noreply.github.com>
2024-08-26 08:14:09 +0530

    Time: 3115 ms (14.37%) | Memory: 13.6 MB (89.75%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         for i in nums:
             if(nums.count(i)==1):
                 return i
