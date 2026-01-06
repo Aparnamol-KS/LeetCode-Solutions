ca49726ba54a884ca828801ea3701d09e5721ada
Aparnamol K S <152758476+Aparnamol-KS@users.noreply.github.com>
2026-01-06 22:23:08 +0530

    Time: 69 ms (80.34%) | Memory: 29.2 MB (5.62%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -3,13 +3,13 @@ class Solution:
         s = 0
         for i in range(k):
             s+=nums[i]
-        maxAvg = window_avg = s/k
+        maxAvg = window_avg = s
         for j in range(k,len(nums)):
             s-=nums[j-k]
             s+=nums[j]
-            window_avg = s/k
+            window_avg = s
             maxAvg = max(maxAvg,window_avg)
-        return maxAvg
+        return maxAvg/k
 
 
 
